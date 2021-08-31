9e4a2b96be05027ab5b66008a82518175fffab7c
Daniel-Amadeus <dajg1995@gmail.com>
2021-08-31 11:04:34 +0200

    init webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: { main: './source/code/index.ts' },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './source/pages/index.pug',
+            // templateParameters: { config, assignments },
+            // chunks: ['index', 'style']
+        })
+    ],
+    mode: 'development',
+    devtool: 'inline-source-map',
+    // output: {
+    //     filename: '[name].js',
+    //     path: path.resolve(__dirname, 'build'),
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: { loader: 'ts-loader' },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.pug$/,
+                use: {
+                    loader: 'pug-loader',
+                    options: { root: path.resolve(__dirname, 'source/pages') }
+                },
+            },
+        ]
+    }
+}
